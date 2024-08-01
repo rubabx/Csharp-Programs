e2e25bb4413eb4e245aee455bdf8be2852ddfc58
Rubab <rubabtariq149@gmail.com>
2024-08-01 23:36:26 +0500

    Add files via upload

---
+++ Constructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        public Program(string name, string location)
+        {
+            Console.WriteLine("My Name is:" + name);
+            Console.WriteLine("My Loaction is:" + location);
+              
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program("Rubab", "Gujranwala");
+            Console.ReadLine();
+        }
+    }
+}
