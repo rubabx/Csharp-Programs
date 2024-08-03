e211ff76aebff9e72032afbe5d06f7010b3edae6
Rubab <rubabtariq149@gmail.com>
2024-08-03 20:36:11 +0500

    Add files via upload

---
+++ StaticConstructor.cs
@@ -0,0 +1,40 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public static string personName;
+        public static int personAge;
+        static Person()
+        {
+            personName = "Ali";
+            personAge = 32;
+            Console.WriteLine("Static Constructor Invoke");
+        }
+
+        public static void GetValue()
+        {
+            Console.WriteLine("Person name is: "+ personName);
+            Console.WriteLine("Person age is: "+ personAge);
+        }
+
+        public Person()
+        {
+            Console.WriteLine("Default Constructor Invoke"  );
+        }
+    }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            Person.GetValue();
+            Person umer = new Person();
+            Console.ReadLine();
+            }
+    }
+}
